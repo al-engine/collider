a38e4814e1a5ae83f52cb5d9690abc84d68aa8ba
zhesha <savzhe@gmail.com>
2022-01-08 12:48:40 +0200

    used absolute position instead of relative

--- src/obstaclesCollision.ts
+++ src/obstaclesCollision.ts
@@ -22,119 +22,119 @@ export function calcPositionFor<T extends GameObjectParams>({
     oldPosition = {
       x: movingObj.oldPosition.x + movingObj.parent.position.x,
       y: movingObj.oldPosition.y + movingObj.parent.position.y,
-    }
+    };
   }
-  oldPosition = {...movingObj.oldPosition};
+  oldPosition = { ...movingObj.oldPosition };
   if (movingObj.speed.x === 0) {
     if (movingObj.speed.y > 0) {
-      movingObj.setAbsolute({y: calcLimitedMoving(
-        movingObj.speed.y,
-        objPosition.y,
-        obstaclePosition.y - movingObj.size.height
-      )});
-    } else {
-      movingObj.setAbsolute({y: calcLimitedMoving(
-        movingObj.speed.y,
-        objPosition.y,
-        obstaclePosition.y + obstacles.size.height
-      )});
-    }
-  } else if (movingObj.speed.y === 0) {
-    if (movingObj.speed.x > 0) {
-      movingObj.setAbsolute({x: calcLimitedMoving(
-        movingObj.speed.x,
-        objPosition.x,
-        obstaclePosition.x - movingObj.size.width
-      )});
-    } else {
-      movingObj.setAbsolute({x: calcLimitedMoving(
-        movingObj.speed.x,
-        objPosition.x,
-        obstaclePosition.x + obstacles.size.width
-      )});
-    }
-  } else if (movingObj.speed.x > 0) {
-    if (movingObj.speed.y > 0) {
-      if (
-        obstaclePosition.x <
-        oldPosition.x + movingObj.size.width
-      ) {
-        movingObj.setAbsolute({y: calcLimitedMoving(
+      movingObj.setAbsolute({
+        y: calcLimitedMoving(
           movingObj.speed.y,
           objPosition.y,
           obstaclePosition.y - movingObj.size.height
-        )});
-      } else if (
-        obstaclePosition.y <
-        oldPosition.y + movingObj.size.height
-      ) {
-        movingObj.setAbsolute({x: calcLimitedMoving(
-          movingObj.speed.x,
-          objPosition.x,
-          obstaclePosition.x - movingObj.size.width
-        )});
-      }
+        ),
+      });
     } else {
-      if (
-        obstaclePosition.x <
-        oldPosition.x + movingObj.size.width
-      ) {
-        movingObj.setAbsolute({y = calcLimitedMoving(
+      movingObj.setAbsolute({
+        y: calcLimitedMoving(
           movingObj.speed.y,
           objPosition.y,
           obstaclePosition.y + obstacles.size.height
-        )});
-      } else if (
-        obstaclePosition.y + obstacles.size.height >
-        oldPosition.y
-      ) {
-        movingObj.setAbsolute({x: calcLimitedMoving(
+        ),
+      });
+    }
+  } else if (movingObj.speed.y === 0) {
+    if (movingObj.speed.x > 0) {
+      movingObj.setAbsolute({
+        x: calcLimitedMoving(
           movingObj.speed.x,
           objPosition.x,
           obstaclePosition.x - movingObj.size.width
-        )});
+        ),
+      });
+    } else {
+      movingObj.setAbsolute({
+        x: calcLimitedMoving(
+          movingObj.speed.x,
+          objPosition.x,
+          obstaclePosition.x + obstacles.size.width
+        ),
+      });
+    }
+  } else if (movingObj.speed.x > 0) {
+    if (movingObj.speed.y > 0) {
+      if (obstaclePosition.x < oldPosition.x + movingObj.size.width) {
+        movingObj.setAbsolute({
+          y: calcLimitedMoving(
+            movingObj.speed.y,
+            objPosition.y,
+            obstaclePosition.y - movingObj.size.height
+          ),
+        });
+      } else if (obstaclePosition.y < oldPosition.y + movingObj.size.height) {
+        movingObj.setAbsolute({
+          x: calcLimitedMoving(
+            movingObj.speed.x,
+            objPosition.x,
+            obstaclePosition.x - movingObj.size.width
+          ),
+        });
+      }
+    } else {
+      if (obstaclePosition.x < oldPosition.x + movingObj.size.width) {
+        movingObj.setAbsolute({
+          y: calcLimitedMoving(
+            movingObj.speed.y,
+            objPosition.y,
+            obstaclePosition.y + obstacles.size.height
+          ),
+        });
+      } else if (obstaclePosition.y + obstacles.size.height > oldPosition.y) {
+        movingObj.setAbsolute({
+          x: calcLimitedMoving(
+            movingObj.speed.x,
+            objPosition.x,
+            obstaclePosition.x - movingObj.size.width
+          ),
+        });
       }
     }
   } else {
     if (movingObj.speed.y < 0) {
-      if (
-        obstaclePosition.x + obstacles.size.width >
-        oldPosition.x
-      ) {
-        movingObj.setAbsolute({y: calcLimitedMoving(
-          movingObj.speed.y,
-          objPosition.y,
-          obstaclePosition.y + obstacles.size.height
-        )});
-      } else if (
-        obstaclePosition.y + obstacles.size.height >
-        oldPosition.y
-      ) {
-        movingObj.setAbsolute({x: calcLimitedMoving(
-          movingObj.speed.x,
-          objPosition.x,
-          obstaclePosition.x + obstacles.size.width
-        )});
+      if (obstaclePosition.x + obstacles.size.width > oldPosition.x) {
+        movingObj.setAbsolute({
+          y: calcLimitedMoving(
+            movingObj.speed.y,
+            objPosition.y,
+            obstaclePosition.y + obstacles.size.height
+          ),
+        });
+      } else if (obstaclePosition.y + obstacles.size.height > oldPosition.y) {
+        movingObj.setAbsolute({
+          x: calcLimitedMoving(
+            movingObj.speed.x,
+            objPosition.x,
+            obstaclePosition.x + obstacles.size.width
+          ),
+        });
       }
     } else {
-      if (
-        obstaclePosition.x + obstacles.size.width >
-        oldPosition.x
-      ) {
-        movingObj.setAbsolute({y: calcLimitedMoving(
-          movingObj.speed.y,
-          objPosition.y,
-          obstaclePosition.y - movingObj.size.height
-        )});
-      } else if (
-        obstaclePosition.y <
-        oldPosition.y + movingObj.size.height
-      ) {
-        movingObj.setAbsolute({x: calcLimitedMoving(
-          movingObj.speed.x,
-          objPosition.x,
-          obstaclePosition.x + obstacles.size.width
-        )});
+      if (obstaclePosition.x + obstacles.size.width > oldPosition.x) {
+        movingObj.setAbsolute({
+          y: calcLimitedMoving(
+            movingObj.speed.y,
+            objPosition.y,
+            obstaclePosition.y - movingObj.size.height
+          ),
+        });
+      } else if (obstaclePosition.y < oldPosition.y + movingObj.size.height) {
+        movingObj.setAbsolute({
+          x: calcLimitedMoving(
+            movingObj.speed.x,
+            objPosition.x,
+            obstaclePosition.x + obstacles.size.width
+          ),
+        });
       }
     }
   }
